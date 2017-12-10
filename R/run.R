4518648b95a6bd40d1975f3a23ae4f986d5f64e2
Robert Ryan Thompson <thomnific@protonmail.com>
2017-12-09 17:12:51 -0700

    Ignore run.R
    Commenting out run.R for now, don't really understand what's going on yet.

--- R/run.R
+++ R/run.R
@@ -5,22 +5,22 @@ library(shiny)
 
 ## load.R contains definitions specifying where and which data to load.
 ## model.R contains the definitions of runModel() and its components.
-## output.R contains the definition of generateBasicOutput().
-source("load.R")
-source("model.R")
-source("output.R")
+## ## output.R contains the definition of generateBasicOutput().
+## source("load.R")
+## source("model.R")
+## source("output.R")
 
 
-## Run 6-digit BACI model for 2014 (including AB) and generate output.
-baci_1996_6digit_2014_ab <- runModel("baci", "1996", "6", 2014, TRUE)
-generateBasicOutput(baci_1996_6digit_2014_ab)
+## ## Run 6-digit BACI model for 2014 (including AB) and generate output.
+## baci_1996_6digit_2014_ab <- runModel("baci", "1996", "6", 2014, TRUE)
+## generateBasicOutput(baci_1996_6digit_2014_ab)
 
-## Run 4-digit BACI model for 2014 (including AB) and generate output.
-baci_1996_4digit_2014_ab <- runModel("baci", "1996", "4", 2014, TRUE)
+## ## Run 4-digit BACI model for 2014 (including AB) and generate output.
+## baci_1996_4digit_2014_ab <- runModel("baci", "1996", "4", 2014, TRUE)
 
-generateBasicOutput(baci_1996_4digit_2014_ab)
+## generateBasicOutput(baci_1996_4digit_2014_ab)
 
-## Run 4-digit OEC model for 2014 (excluding AB).
-oec_1996_6digit_2014_ab <- runModel("oec", "1996", "4", 2014, FALSE)
+## ## Run 4-digit OEC model for 2014 (excluding AB).
+## oec_1996_6digit_2014_ab <- runModel("oec", "1996", "4", 2014, FALSE)
 
 runApp("simple-app")
