6c60c1bb085d00f8768a374ce9deceb29180bc1e
Robert Ryan Thompson <thomnific@protonmail.com>
2017-12-09 17:04:28 -0700

    Change to source methods in run.R
    Hmm, package won't install with devtools -- maybe I need to change the paths
    in the source statements here?

--- R/run.R
+++ R/run.R
@@ -1,25 +1,15 @@
-## Clear the model workspace.
-rm(list = ls())
-
-## This line needs to be changed to the appropriate library path.
-.libPaths("C:/Users/smyrna/Documents/R/win-library/3.3")
-
 ## This version of the model depends on some of the tidyverse packages.
 library(dplyr)
 library(ggplot2)
 library(shiny)
 
-## The wideScreen function is useful for debugging.
-wideScreen <- function(howWide=Sys.getenv("COLUMNS")) {
-  options(width=as.integer(howWide))
-}
-
 ## load.R contains definitions specifying where and which data to load.
 ## model.R contains the definitions of runModel() and its components.
 ## output.R contains the definition of generateBasicOutput().
-source("load.R")
-source("model.R")
-source("output.R")
+source(file.path(path.package("ecxplor"), "R", "load.R"))
+source(file.path(path.package("ecxplor"), "R", "model.R"))
+source(file.path(path.package("ecxplor"), "R", "output.R"))
+
 
 ## Run 6-digit BACI model for 2014 (including AB) and generate output.
 baci_1996_6digit_2014_ab <- runModel("baci", "1996", "6", 2014, TRUE)
