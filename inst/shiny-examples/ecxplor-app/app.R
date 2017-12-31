f1f3d76cfa7d20c32cc97e426b8b678d4dddee1c
Robert Ryan Thompson <thomnific@protonmail.com>
2017-12-30 18:32:08 -0700

    Start to clean up the Shiny app
    Now that I can run and reload the model reliably, it's time to
    start actually fixing up the Shiny app. (Decided to just stick
    with the basic version for Gareth for now.)

--- inst/shiny-examples/ecxplor-app/app.R
+++ inst/shiny-examples/ecxplor-app/app.R
@@ -1,29 +1,26 @@
-library(ggplot2)
-library(dplyr)
-library(network)
-library(networkD3)
-library(sna)
-library(GGally)
-library(shiny)
-
 library(ecxplor)
 
-choices <- rownames(model_object[["proximity"]])
+library(networkD3)
+library(shiny)
+
+model_obj <- loadModel("baci", "2007", "6", 2014, TRUE)
+
+choices <- rownames(model_obj[["proximity"]])
 choices_panel <- tbl_df(data.frame(product = choices,
                                    stringsAsFactors = FALSE))
 
-product_info_panel <- model_object[["product_info"]]
+product_info_panel <- model_obj[["product_info"]]
 
 choices_info_panel <- tbl_df(merge(choices_panel, product_info_panel, all.x = TRUE))
 choices_to_display <- choices_info_panel[["product"]]
 names(choices_to_display) <- choices_info_panel[["name"]]
-unnamed_choices_to_display <- choices_to_display[is.na(names(choices_to_display))]3
+unnamed_choices_to_display <- choices_to_display[is.na(names(choices_to_display))]
 named_choices_to_display <- choices_to_display[!is.na(names(choices_to_display))]
 
 names(unnamed_choices_to_display) <- unnamed_choices_to_display
 final_choices_to_display <- c(named_choices_to_display, unnamed_choices_to_display)
 
-exports_panel <- model_object[["exports"]]
+exports_panel <- model_obj[["exports"]]
 
 ui <- fluidPage(
 
@@ -32,23 +29,6 @@ ui <- fluidPage(
     sidebarLayout(
 
         sidebarPanel(
-
-            selectInput(inputId = "data_tag",
-                        label = "Data source:",
-                        choices = c("OEC", "BACI"),
-                        multiple = FALSE),
-            selectInput(inputId = "hs_rev_year",
-                        label = "HS revision year:",
-                        choices = c("1992", "1996", "2002", "2007"),
-                        multiple = FALSE),
-            selectInput(inputId = "hs_rev_digit",
-                        label = "Product scheme digits:",
-                        choices = c("4", "6"),
-                        multiple = FALSE),
-            selectInput(inputId = "input_year",
-                        label = "Product scheme digits:",
-                        choices = c("4", "6"),
-                        multiple = FALSE),
 
             selectInput(inputId = "focus_product",
                         label = "Product focus:",
@@ -83,12 +63,10 @@ server <- function(input, output) {
         input_year <- 2014
         phi_cutoff <- 0.55
 
-        model_object <- runModel("baci", hs_rev_year, hs_rev_digit, input_year, TRUE)
-
     })
 
     output$product_space_plot <- renderForceNetwork(        {
-            prepared_model <- prepareFocusedModelObject(model_object, input$focus_product, input$search_depth)
+            prepared_model <- prepareFocusedModelObject(model_obj, input$focus_product, input$search_depth)
             forceNetwork(Links = prepared_model[["d3_edges"]],
                          Nodes = prepared_model[["d3_nodes"]],
                          Source="from",
